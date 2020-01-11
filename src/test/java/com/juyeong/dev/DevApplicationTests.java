ace0a51203e5637f6211822ac1cae57421cf4c69
LTTTTTE <10100aaaaa@gmail.com>
2020-01-11 13:43:51 +0900

    init project

---
+++ src/test/java/com/juyeong/dev/DevApplicationTests.java
@@ -0,0 +1,16 @@
+package com.juyeong.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DevApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
